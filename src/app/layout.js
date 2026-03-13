f27bd930014a4d63a1309a2a0433e72999e75244
Nathan Silva <nathansilva.dev@gmail.com>
2026-03-13 15:56:58 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,24 @@
 import "./globals.css";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18013965754"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18013965754');
+          `}
+        </Script>
+      </head>
       <body className="bg-[linear-gradient(to_bottom,_#03001B,_#060416)]">
         {children}
       </body>
